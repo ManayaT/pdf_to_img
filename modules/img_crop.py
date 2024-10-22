81279084618a9bab11a31f28a0486d89b82b6808
ManayaT <waddle-dee.1210@docomo.ne.jp>
2024-10-22 10:19:23 +0900

    add_crop_filename

--- modules/img_crop.py
+++ modules/img_crop.py
@@ -1,7 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
 
-def crop_image(image_dir, output_dir, file_name):
+def crop_code(image_dir, output_dir, file_name):
     image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.gif')
 
     if not os.path.exists(image_dir):
@@ -19,6 +19,34 @@ def crop_image(image_dir, output_dir, file_name):
 
                 # 領域の座標 (左, 上, 右, 下)
                 crop_area = (118, 0, 1600, 2195)
+
+                cropped_image = image.crop(crop_area)
+
+                cropped_image.save(f'{output_dir}{file_name}_{i+1}.png', "PNG")
+
+        return True
+    except Exception as e:
+        #print(f"{e}")
+        return False
+
+def crop_name(image_dir, output_dir, file_name):
+    image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.gif')
+
+    if not os.path.exists(image_dir):
+        exit()
+
+    if not os.path.exists(output_dir):
+        exit()
+
+    try:
+        for i, filename in enumerate(os.listdir(image_dir)):
+            if filename.lower().endswith(image_extensions):
+                image_path = os.path.join(image_dir, filename)
+
+                image = Image.open(image_path)
+
+                # 領域の座標 (左, 上, 右, 下)
+                crop_area = (0, 0, 850, 110)
 
                 cropped_image = image.crop(crop_area)
 
