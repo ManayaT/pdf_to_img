1c98f4cfb123b5b192edc09b9403ba4e2d8d1740
ManayaT <waddle-dee.1210@docomo.ne.jp>
2024-10-24 09:02:58 +0900

    add_move_file_func

--- modules/img_crop.py
+++ modules/img_crop.py
@@ -1,17 +1,19 @@
 from PIL import Image, ImageDraw
+from natsort import natsorted as nat
 import os
 
 image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.gif')
 
 def crop(image_dir, output_dir, file_name, crop_area):
+
     if not os.path.exists(image_dir):
-        exit()
+        return False
 
     if not os.path.exists(output_dir):
-        exit()
+        return False
 
     try:
-        for i, filename in enumerate(os.listdir(image_dir)):
+        for i, filename in enumerate(nat(os.listdir(image_dir))):
             if filename.lower().endswith(image_extensions):
                 image_path = os.path.join(image_dir, filename)
 
@@ -19,7 +21,8 @@ def crop(image_dir, output_dir, file_name, crop_area):
 
                 cropped_image = image.crop(crop_area)
 
-                cropped_image.save(f'{output_dir}{file_name}_{i+1}.png', "PNG")
+                output_path = f'{output_dir}{file_name}_{i+1}.png'
+                cropped_image.save(output_path.replace("\n", ""), "PNG")
 
         return True
     except Exception as e:
--- modules/pdf_to_img.py
+++ modules/pdf_to_img.py
@@ -4,17 +4,19 @@ import os
 def pdf_to_images(pdf_path, output_dir, file_name):
 
         if not ".pdf" in pdf_path:
-            exit()
+            return False
 
         if not os.path.exists(pdf_path):
-            exit()
+            return False
 
         if not os.path.exists(output_dir):
-            exit()
+            return False
 
         try:
             images = convert_from_path(pdf_path)
             for i, image in enumerate(images):
+
+                
                 image.save(f'{output_dir}{file_name}_{i+1}.png', 'PNG')
 
             return True
